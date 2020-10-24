cc8daa09f383d9795cb05d1101cfab6454ab4490
lorberta <lorberta@github.com>
2020-10-24 12:52:27 +0200

    added use-context config in kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,6 +8,7 @@ dockerpath=lorberta/cast:v1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl config use-context minikube
 kubectl create deployment --image $dockerpath cast --port=80
 
 # Step 3:
